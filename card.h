33b2fe4523137ec6873f755d83783861ee0fbca9
kentonla <116829705+kentonla@users.noreply.github.com>
2024-09-21 14:29:10 -0700

    Added honor statement

--- card.h
+++ card.h
@@ -1,3 +1,10 @@
+// Name: Kenton La
+// CECS 325-01
+// Prog 1 - One Card War
+// Sep 18, 2024
+
+// I certify that this program is my own original work. I did not copy any part of this program from any other source.
+// I further certify that I typed each and every line of code in this program.
 #ifndef CARD_H
 #define CARD_H
 #include <iostream>
